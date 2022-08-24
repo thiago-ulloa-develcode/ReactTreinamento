8b98a9ade3447474cca02f919420b3aa0d91addd
thiago-ulloa-develcode <thiago.ulloa@develcode.com.br>
2022-08-24 11:33:17 -0300

    adaptando mascara de entrada

--- src/pages/register-page/index.js
+++ src/pages/register-page/index.js
@@ -21,16 +21,10 @@ function RegisterPage() {
     );
 }
 function Mascara() {
-const input = document.getElementById('cnpj')
-    let inputlength = input.value.length
-
-    if (inputlength === 2 || inputlength === 6) {
-        input.value += '.'
-    } else if (inputlength === 10) {
-        input.value += '/'
-    } else if (inputlength === 15) {
-        input.value += '-'
-    }
+    document.getElementById('cnpj').addEventListener('input', function (e) {
+        var x = e.target.value.replace(/\D/g, '').match(/(\d{0,2})(\d{0,3})(\d{0,3})(\d{0,4})(\d{0,2})/);
+        e.target.value = !x[2] ? x[1] : x[1] + '.' + x[2] + '.' + x[3] + '/' + x[4] + (x[5] ? '-' + x[5] : '');
+      });
 }
 
 
