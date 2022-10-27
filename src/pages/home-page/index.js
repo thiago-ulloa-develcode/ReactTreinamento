5335d08c11dce225c77e13242097a18ba9038116
thiago-ulloa-develcode <86962607+thiago-ulloa-develcode@users.noreply.github.com>
2022-10-27 00:00:11 -0300

    Organizando código e criando função toggleText

--- src/pages/home-page/index.js
+++ src/pages/home-page/index.js
@@ -1,5 +1,5 @@
 import "./styles.css";
-import React from "react";
+import React, { useState } from "react";
 import menuImage from "./images/menubar.png";
 import profileImage from "./images/profileicon.png";
 import homeBtnImage from "./images/homebutton.png";
@@ -7,10 +7,12 @@ import { useLocation } from "react-router-dom";
 
 function HomePage() {
   const [btnState, setBtnState] = React.useState(false);
+  // Informações do restaurante
+  const [resName, setResName] = React.useState("");
+  const [resId, setResId] = React.useState("");
 
   const { state } = useLocation();
   const { token } = state;
-  console.log(state);
 
   function openNav() {
     setBtnState((btnState) => !btnState);
@@ -18,8 +20,12 @@ function HomePage() {
 
   React.useEffect(() => {
     getRestaurant();
+    // getRestaurantEvaluation();
   }, []);
 
+  let toggleClassCheck = btnState ? "-open" : "";
+
+  // Função GET com utilização do token
   const getRestaurant = () => {
     fetch("https://develfood-3.herokuapp.com/restaurant/auth", {
       method: "GET",
@@ -29,11 +35,41 @@ function HomePage() {
       },
     })
       .then((response) => response.json())
-      .then((json) => console.log(json))
+      .then((response) => onFetchSucess(response))
       .catch((err) => console.log("Erro de solicitação", err));
   };
 
-  let toggleClassCheck = btnState ? "-open" : "";
+  /* async function getRestaurantEvaluation() {
+    await fetch(
+      "https://develfood-3.herokuapp.com/restaurantEvaluation/" + { resId },
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json; charset=utf8",
+          Authorization: "Bearer " + token,
+        },
+      }
+    )
+      .then((response) => response.json())
+      .then((response) => console.log(response));
+  } */
+
+  // Separar as informações do restaurante com o resultado do GET
+  const onFetchSucess = (resData) => {
+    setResName(resData.name);
+    setResId(resData.id);
+    console.log(resData);
+  };
+
+  // função para alternar texto de acordo com o menu
+  function toggleText() {
+    var text = document.getElementById("profileText");
+    if (text.style.display === "none") {
+      text.style.display = "block";
+    } else {
+      text.style.display = "none";
+    }
+  }
 
   return (
     <div className="homepage">
@@ -44,7 +80,10 @@ function HomePage() {
               src={menuImage}
               id="menubutton"
               alt="menubutton"
-              onClick={openNav}
+              onClick={() => {
+                openNav();
+                toggleText();
+              }}
             />
           </div>
           <div className="homebutton">
@@ -62,11 +101,17 @@ function HomePage() {
               id="profilebutton"
               onClick={() => window.location.assign("/profile-page")}
             />
+            <p
+              id="profileText"
+              style={{ display: "none", color: "white", fontSize: "20px" }}
+            >
+              Perfil
+            </p>
           </div>
         </div>
         <div className="resHome">
           <div className="top-align">
-            <b>Nome do Resturante</b>
+            <h1>{resName}</h1>
           </div>
           <div className="center-align">
             <div className="promotionBox">
